a8747c21dde10820c0660d0300e3764052e69c6e
Michiel Vissenberg <michielvissenberg@Panencos-MacBook-Pro.local>
2026-08-03 13:23:22 +0200

    cleaned up main

--- src/controllers/users/handlers/getList.handler.js
+++
@@ -1,8 +0,0 @@
-const { UserStore } = require("./user.store.js");
-
-const getList = (req, res, next) => {
-    const users = UserStore.find("");
-    res.json(users);
-};
-
-module.exports = { getList };
--- src/server.js
+++
@@ -1,5 +0,0 @@
-const { App } = require("./app.js");
-
-const app = new App();
-app.listen();
-
